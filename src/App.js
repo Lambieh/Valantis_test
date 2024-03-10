9e70848b444b41233c27001767d5f0ae6d338d73
Lambieh <95087309+Lambieh@users.noreply.github.com>
2024-03-11 02:20:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
   const getProductIds = async (params) => {
     try {
-      const response = await fetch("http://api.valantis.store:40000/", {
+      const response = await fetch("https://api.valantis.store:41000/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -38,7 +38,7 @@ function App() {
 
   const getProducts = async (ids) => {
     try {
-      const response = await fetch("http://api.valantis.store:40000/", {
+      const response = await fetch("https://api.valantis.store:41000/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
